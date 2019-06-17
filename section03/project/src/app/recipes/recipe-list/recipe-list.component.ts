79f0bafa1b94899472b487e750b52439e8287cca
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-17 13:51:01 -0700

    update Forms
    Update layout and details of each forms

--- section03/project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ section03/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.bbcgoodfood.com/sites/default/files/categories/categories-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new Recipe('Persian Recipe', 
+      'This is a Persian Recipe',
+      'https://www.recipetineats.com/wp-content/uploads/2018/08/Persian-Week-Feast-1.jpg')
+  ];
 
   constructor() { }
 
--- section03/project/src/app/shopping-list/shopping-list.component.ts
+++ section03/project/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -7,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('Meat', 1),
+    new Ingredient('Tomatoes', 2),
+    new Ingredient('potatoes', 2)
+  ];
   constructor() { }
 
   ngOnInit() {
