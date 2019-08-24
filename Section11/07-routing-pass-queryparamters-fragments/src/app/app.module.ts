38a3d8526bd06497a3c476b65450affc2e93818c
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-08-23 17:34:01 -0700

    routing

--- Section11/07-routing-pass-queryparamters-fragments/src/app/app.module.ts
+++ Section11/07-routing-pass-queryparamters-fragments/src/app/app.module.ts
@@ -14,10 +14,11 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
-  { path: '', component: UserComponent },
+  { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent }
+  { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent}
 ];
 
 @NgModule({
---
+++ Section11/09-routing-setup-child-nested-route/src/app/servers/server/server.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // id is string here and we need to convert it to number, the + sign will do this for us
+    const id = +this.route.snapshot.params['id'];
+
+    console.log(id);
+    
+    this.server = this.serversService.getServer(id);
+
+    console.log(this.server);
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
+  }
+
+}
