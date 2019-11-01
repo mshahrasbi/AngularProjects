923f3e3df68ed037ba28501b23ec0bb871de49c4
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-11-01 16:28:46 -0700

    handling forms in angular app

---
+++ section15/08-forms-submitting-ngModel-with-two-way-binfing/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  defaultQuestion = 'pet';
+  answer = '';
+  
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
