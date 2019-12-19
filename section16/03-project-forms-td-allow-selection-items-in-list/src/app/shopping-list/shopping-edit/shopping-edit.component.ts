8aa003b70c857de04aa787c0e0176130c34f19e0
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-12-19 16:10:59 -0700

    recipe project

---
+++ section16/03-project-forms-td-allow-selection-items-in-list/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.subscription = this.slService
+      .startedEditting
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onAddItem(form: NgForm) {
+    const ingName = form.value.name;
+    const ingAmount = form.value.amount;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+
+}
---
+++ section16/04-project-forms-td-load-shopping-list-into-form/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,53 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  edittedItem: Ingredient;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.subscription = this.slService
+      .startedEditting
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+        this.edittedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.edittedItem.name,
+          amount: this.edittedItem.amount
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onAddItem(form: NgForm) {
+    const ingName = form.value.name;
+    const ingAmount = form.value.amount;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+
+}
