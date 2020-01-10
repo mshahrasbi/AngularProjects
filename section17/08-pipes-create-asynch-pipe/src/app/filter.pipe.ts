456e220c705925323ddddc582f71eda8c859589b
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2020-01-09 17:35:52 -0700

    pipes

---
+++ section17/08-pipes-create-asynch-pipe/src/app/filter.pipe.ts
@@ -0,0 +1,26 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+
+
+@Pipe({
+    name: 'filter',
+    pure: false
+})
+export class FilterPipe implements PipeTransform {
+
+    transform(value: any, filterString: string, propName: string): any {
+        if (value.length === 0 || filterString === '') {
+            return value;
+        }
+
+        const resultArray = [];
+        for (const item of value) {
+            if (item[propName] === filterString) {
+              resultArray.push(item);
+            }
+        }
+
+        return resultArray;
+    }
+}
