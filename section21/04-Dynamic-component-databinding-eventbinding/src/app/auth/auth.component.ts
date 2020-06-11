da7e43ef4d16b68241cca75f023ea1297ab5cd9b
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2020-06-11 01:29:18 -0700

    dynamic component

--- section21/04-Dynamic-component-databinding-eventbinding/src/app/auth/auth.component.ts
+++ section21/04-Dynamic-component-databinding-eventbinding/src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
@@ -11,12 +11,14 @@ import { PlaceholderDirective } from '../shared/placeholder/placehodler.directiv
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = false;
   isLoading = false;
   error: string = null;
 
   @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
 
   constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 
@@ -65,12 +67,32 @@ export class AuthComponent {
     this.error = null;
   }
 
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
+
   private showError(message: string) {
     // const alertCmp = new AlertComponent();
     const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
   
     const hostViewComponentRef = this.alertHost.viewContainerRef;
     hostViewComponentRef.clear();
-    hostViewComponentRef.createComponent(alertCmpFactory);
+    const componentRef = hostViewComponentRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = message;
+
+    /**
+     * remember that close in here is just an eventEmitter, now we know that we should use eventemitter bascially
+     * only here when it is decorated with @output and whenever we manually subscribe to something you would
+     * want to use a subject, here is basically the only execption to that rule. Since this is a component where 
+     * we have @output, because we could use this component by a selector as well, we have an eventEmitter and yet
+     * here, we manually want to subscribe and so we subscribe to this, so whenever this fires we want to run this 
+     * anaynoumous function. 
+     */
+    this.closeSub = componentRef.instance.close.subscribe( () => {
+      this.closeSub.unsubscribe();
+      hostViewComponentRef.clear();
+    });
   }
 }
---
+++ section21/04-Dynamic-component-databinding-eventbinding/src/app/auth/auth.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot):
+                        | boolean
+                        | UrlTree
+                        | Promise<boolean | UrlTree>
+                        | Observable<boolean | UrlTree> {
+
+        return this.authService.user.pipe(take(1), map(user => {
+            const isAuth = !!user;
+
+            console.log('[AuthGuard] canActivate: ', isAuth);
+
+            if (isAuth) {
+                return true;
+            }
+
+            return this.router.createUrlTree(['/auth']);
+
+        }));
+    }
+}
