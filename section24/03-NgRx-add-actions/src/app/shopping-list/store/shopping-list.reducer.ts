0090088c31c7280764aee4e422735d156f1a1b21
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2020-08-21 15:34:56 -0700

    using NgRx

---
+++ section24/03-NgRx-add-actions/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,26 @@
+
+import { Action } from '@ngrx/store';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import {ADD_INGREDIENT} from './shopping-list.action';
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ]
+};
+
+export function shoppingListReducer(state = initialState, action: Action) {
+
+    switch (action.type) {
+        case ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: [
+                    ...state.ingredients,
+                    action
+                ] // can not pass the action like this
+            };
+    }
+}
---
+++ section24/06-NgRx-dispatch-actions/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({
+      shoppingList: shoppingListReducer
+    }),
+    SharedModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
